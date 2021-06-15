36b367f2e0eb338c6d1f72c6d131669a39218045
Vishnu Priya Reddy <83069932+priya2404@users.noreply.github.com>
2021-06-15 12:29:14 +0530

    Add files via upload
    demanevent is bal file and DemandEvent IS DAL FILE

---
+++ DemandEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BALEventMgt;
+
+namespace DALeEventMgt
+{
+    public class DemandEvent
+    {
+        SqlConnection cn = new SqlConnection("server=PAKANATI\\PAKANATI;Integrated Security=true;database=EventManagement");
+
+        // cmd.CommandType = System.Data.CommandType.StoredProcedure;
+        public DemandEvent demand()
+        {
+            SqlCommand cmd = new SqlCommand("[dbo].[demand]", cn);
+            //SqlCommand cmd = new SqlCommand("demand", cn);
+            cn.Open();
+            demanevent de = new demanevent();
+            try
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.HasRows)
+                {
+                    dr.Read();
+                    de.events = Convert.ToString(dr["events"]);
+                    de.users = Convert.ToString(dr["users"]);
+
+                }
+                else
+                {
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            cn.Close();
+            return de;
+        }
+
+
+    }
+}
+
---
+++ demanevent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BALEventMgt
+{
+    public class demanevent
+    {
+        public string events { get; set; }
+        public string users { get; set; }
+    }
+}
